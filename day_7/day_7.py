6aa43dc1ace330588790f94f99e6a35d5f8e02a0
johanan-daniel <70776118+johanan-daniel@users.noreply.github.com>
2024-12-10 23:13:25 -0500

    day 7 initial commit

---
+++ day_7/day_7.py
@@ -0,0 +1,8 @@
+"""
+- parse input into two arrays
+    - 1 for test values
+    - 1 for array of numbers in each equation
+
+- make multiples arrays for order of operators:
+    - use backtracking to to make multiple permutations of the two possibilities
+"""
