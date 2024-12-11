fa02a7f702a1fbc6c7387509d5e2d3a14cf562cf
johanan-daniel <70776118+johanan-daniel@users.noreply.github.com>
2024-12-11 17:36:09 -0500

    part 2

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -17,6 +17,7 @@ BASE_PATH = (os.getenv('BASE_PATH') or "") + f"day_{DAY}/"
 INPUT_FILE = BASE_PATH + f"day_{DAY}_input.txt"
 EXAMPLE_FILE = BASE_PATH + f"day_{DAY}_example.txt"
 EXAMPLE_FILE_2 = BASE_PATH + f"day_{DAY}_example_2.txt"
+EXAMPLE_FILE_3 = BASE_PATH + f"day_{DAY}_example_3.txt"
 
 def parse_input_file(filename: str):
     with open(filename, 'r') as file:
@@ -28,7 +29,6 @@ def parse_input_file(filename: str):
 matrix = parse_input_file(INPUT_FILE)
 num_rows = len(matrix)
 num_cols = len(matrix[0])
-# print(matrix)
 
 def find_antennas():
     antennas = defaultdict(list)
@@ -44,31 +44,49 @@ def find_antennas():
 def in_bounds(coord):
     return coord[0] >= 0 and coord[0] < num_rows and coord[1] >= 0 and coord[1] < num_cols
 
-def part_1():
+def main(part: int):
     antennas = find_antennas()
-    
     antinodes = set()
     
     for type in antennas:
         locations = antennas[type]
         
+        # loops over each pair
         for i in range(len(locations)):
             for j in range(i + 1, len(locations)):
-                dx = locations[j][1] - locations[i][1]
-                dy = locations[j][0] - locations[i][0]
+                coord1 = locations[i]
+                coord2 = locations[j]
                 
-                up = (locations[i][0] - dy, locations[i][1] - dx)
-                down = (locations[j][0] + dy, locations[j][1] + dx)
-                # print(locations[i], locations[j], '-', end=' ')
-                if in_bounds(up):
-                    # print(up, end=' ')
+                # coord[0] == y, coord[1] == x
+                dx = coord2[1] - coord1[1]
+                dy = coord2[0] - coord1[0]
+                
+                if part == 1:
+                    # skip existing antenna
+                    up = (coord1[0] - dy, coord1[1] - dx)
+                    down = (coord2[0] + dy, coord2[1] + dx)
+                elif part == 2:
+                    # start at opposite antenna to include them
+                    down = (coord1[0] + dy, coord1[1] + dx)
+                    up = (coord2[0] - dy, coord2[1] - dx)
+                
+                while in_bounds(up):
                     antinodes.add(up)
-                if in_bounds(down):
-                    # print(down, end='')
+                    up = (up[0] - dy, up[1] - dx)
+                    if part == 1:
+                        # only go one distance out
+                        break
+                while in_bounds(down):
                     antinodes.add(down)
-                # print()
+                    down = (down[0] + dy, down[1] + dx)
+                    if part == 1:
+                        # only go one distance out
+                        break
     
     return len(antinodes)
 
-part_1_res = part_1()
+part_1_res = main(part=1)
 print('part 1:', part_1_res)
+
+part_2_res = main(part=2)
+print('part 2:', part_2_res)
