49bbaa17558a096298d7106be8d2cc3f710eaacf
johanan-daniel <70776118+johanan-daniel@users.noreply.github.com>
2024-12-20 20:35:17 -0500

    day 14 done

--- day_14/day_14.py
+++ day_14/day_14.py
@@ -35,18 +35,31 @@ def parse_input_file(filename: str):
 
 robots = parse_input_file(INPUT_FILE)
 
-def main(y_max, x_max, start_seconds, max_seconds):
+def main(y_max, x_max, start_seconds, max_seconds, part):
     quadrants = defaultdict(int)
     x_mid = x_max // 2
     y_mid = y_max // 2
     total = 1
     
+    if part == 1:
+        start_seconds = max_seconds
+    
     for robot in robots:
         x_pos = (robot[POS][X] + start_seconds * robot[VEL][X]) % x_max
         y_pos = (robot[POS][Y] + start_seconds * robot[VEL][Y]) % y_max
         
-        robot[POS][X] = x_pos
-        robot[POS][Y] = y_pos
+        if part == 1:
+            if (x_pos == x_mid or y_pos == y_mid):
+                continue
+            quadrants[(x_pos // (x_mid + 1), y_pos // (y_mid + 1))] += 1
+        else:
+            robot[POS][X] = x_pos
+            robot[POS][Y] = y_pos
+    
+    if part == 1:
+        for value in quadrants.values():
+            total *= value
+        return total
 
     
     with open(BASE_PATH + 'output.txt', 'a') as file:
@@ -72,12 +85,10 @@ def main(y_max, x_max, start_seconds, max_seconds):
             for row in matrix:
                 file.write(''.join(row) + '\n')
             file.write('\n\n')
-            
-    for value in quadrants.values():
-        total *= value
-    return total
     
 
-part_1_res = main(103, 101, 5*(10**3), 10*(10**3))
-# part_1_res = main(7, 11, 100)
+part_1_res = main(103, 101, 0, 100, part = 1)
+# part_1_res = main(7, 11, 0, 100, part = 1)
 print('part 1:', part_1_res)
+
+part_2_res = main(103, 101, 0, 100, part = 2)
